a464f5e170312437a72f84e428d76c564011c547
guiguishuaige <3044669728@qq.com>
2025-07-19 08:52:18 +0800

    Update config.js
    更改头像，使用图片外链

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {
     icon: '/favicon.ico'   //网页图标，支持外链
   },
 
-  avatar: "/img/avatar.jpg", // 头像
+  avatar: "https://cfbed.1314883.xyz/file/1752886284573_xiangxinweilai.jpg", // 头像
   welcometitle: "Hi, I'm Leleo", // 标题
 
   // 颜色配置
